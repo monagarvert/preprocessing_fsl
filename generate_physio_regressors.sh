a497c0bea56cdab30dc56150e15e428c65232164
Mona Garvert <mgarvert@jalapeno00.fmrib.ox.ac.uk>
2017-07-12 13:12:52 +0100

    preprocessing scripts

---
+++ generate_physio_regressors.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+subj=1
+initials=ts
+
+# Run Pnm
+for session in 0 1 2
+do
+    for run in 1 2 3 4
+    do 
+	echo Session $session
+	echo Run $run
+	dir=/home/fs0/mgarvert/scratch/ManyMaps/imagingData/Subj_$subj/session_$session
+	mkdir $dir/run_$run/physio/
+
+	#/opt/fmrib/fsl/bin/fslFixText $dir/physio/Subj_${subj}_ts_session${session}_run$run.txt $dir/run_$run/physio/physio_input.txt
+	/opt/fmrib/fsl/bin/pnm_stage1 -i $dir/run_$run/physio/physio_input.txt -o $dir/run_$run/physio/physio -s 50 --tr=1.235 --rvt --smoothcard=0.1 --smoothresp=0.1 --resp=2 --cardiac=4 --trigger=3 
+	/opt/fmrib/fsl/bin/popp -i $dir/run_$run/physio/physio_input.txt -o $dir/run_$run/physio/physio -s 50 --tr=1.235 --rvt --smoothcard=0.1 --smoothresp=0.1 --resp=2 --cardiac=4 --trigger=3
+       
+	/opt/fmrib/fsl/bin/pnm_evs -i $dir/run_$run/funct_block_$run.nii.gz -o $dir/run_$run/physio/physio_ev --tr=1.235 -c $dir/run_$run/physio/physio_card.txt --oc=3 -r $dir/run_$run/physio/physio_resp.txt --or=3 --rvt=$dir/run_$run/physio/physio_rvt.txt --slicetiming=$dir/scripts/sliceOrder$run.txt
+
+	$dir/run_$run/physio/physio_pnm_stage2
+
+	ls $dir/run_$run/physio/*.nii.gz > $dir/run_$run/physio/evlist.txt
+    done
+done
---
+++ run_preprocessing.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+subj=Subj_1
+initials=ts
+
+# Run Pnm
+for session in 2 
+do
+    for run in 1 2 3 4
+    do 
+	echo Session $session Run $run
+
+	dir=/home/fs0/mgarvert/scratch/ManyMaps/imagingData/$subj/session_$session
+       	nvols=`fslnvols $dir/run_$run/funct_block_$run.nii.gz`
+
+	
+	../setFEAT /home/fs0/mgarvert/scratch/ManyMaps/imagingData/scripts/preprocess_template.fsf $dir/run_$run/preprocess.fsf -replace Subj_1 $subj -replace session_0 session_$session -replace run_1 run_$run -replace NVOLS $nvols -replace sliceOrder1 sliceOrder$run -replace block_1 block_$run
+
+	feat $dir/run_$run/preprocess.fsf
+    done
+done
